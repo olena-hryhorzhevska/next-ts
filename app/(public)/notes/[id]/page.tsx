1ab91611f4f701fe9c495cf0ad3853551150aa49
Olena Hryhorzhevska <elenahry8@gmail.com>
2026-05-11 17:49:36 -0400

    meta tags

--- app/(public)/notes/[id]/page.tsx
+++ app/(public)/notes/[id]/page.tsx
@@ -6,6 +6,30 @@ type Props = {
   params: Promise<{ id: string }>;
 };
 
+export async function generateMetadata({ params }: Props) {
+  const { id } = await params;
+  const note = await getSingleNote(id);
+  return {
+    title: `Note: ${note.title}`,
+    description: note.content.slice(0, 30),
+    openGraph: {
+      title: `Note: ${note.title}`,
+      description: note.content.slice(0, 100),
+      url: `https://next-ts-smoky.vercel.app/notes/${id}`,
+      siteName: "NoteLab",
+      images: [
+        {
+          url: "https://ac.goit.global/fullstack/react/og-meta.jpg",
+          width: 1200,
+          height: 630,
+          alt: "NoteLab - A simple note-taking app built with Next.js 13 and TypeScript.",
+        },
+      ],
+      type: "website",
+    },
+  };
+}
+
 const NoteDetails = async ({ params }: Props) => {
   const { id } = await params;
   const queryClient = new QueryClient();
--- app/(public)/profile/edit/page.tsx
+++ app/(public)/profile/edit/page.tsx
@@ -1,9 +1,14 @@
+// app/profile/edit/page.tsx
+
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Edit Profile",
+  description: "Edit your user details and settings",
+};
+
 const EditProfile = () => {
-  return (
-    <div>
-      <h1>Edit Profile</h1>
-    </div>
-  )
-}
+  return <div>EditProfile</div>;
+};
 
 export default EditProfile;
