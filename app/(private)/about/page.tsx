75757b91de68f0335c69ec0b430e244438832ad9
Olena Hryhorzhevska <elenahry8@gmail.com>
2026-05-11 21:05:57 -0400

    meta tags correction

--- app/(private)/about/page.tsx
+++ app/(private)/about/page.tsx
@@ -1,11 +1,6 @@
 // app/about/page.tsx
 
-import { Metadata } from "next";
 import { redirect } from "next/navigation";
-
-export const metadata: Metadata = {
-  title: "About page",
-};
 
 const AboutPage = () => {
   redirect("/");
--- app/(public)/notes/[id]/page.tsx
+++ app/(public)/notes/[id]/page.tsx
@@ -1,41 +1,21 @@
-import { getSingleNote } from '@/app/lib/api';
-import { QueryClient, HydrationBoundary, dehydrate } from '@tanstack/react-query';
-import NoteDetailsClient from './NoteDetails.client';
+import { getSingleNote } from "@/app/lib/api";
+import {
+  QueryClient,
+  HydrationBoundary,
+  dehydrate,
+} from "@tanstack/react-query";
+import NoteDetailsClient from "./NoteDetails.client";
 
 type Props = {
   params: Promise<{ id: string }>;
 };
-
-export async function generateMetadata({ params }: Props) {
-  const { id } = await params;
-  const note = await getSingleNote(id);
-  return {
-    title: `Note: ${note.title}`,
-    description: note.content.slice(0, 30),
-    openGraph: {
-      title: `Note: ${note.title}`,
-      description: note.content.slice(0, 100),
-      url: `https://next-ts-smoky.vercel.app/notes/${id}`,
-      siteName: "NoteLab",
-      images: [
-        {
-          url: "https://ac.goit.global/fullstack/react/og-meta.jpg",
-          width: 1200,
-          height: 630,
-          alt: "NoteLab - A simple note-taking app built with Next.js 13 and TypeScript.",
-        },
-      ],
-      type: "website",
-    },
-  };
-}
 
 const NoteDetails = async ({ params }: Props) => {
   const { id } = await params;
   const queryClient = new QueryClient();
 
   await queryClient.prefetchQuery({
-    queryKey: ['note', id],
+    queryKey: ["note", id],
     queryFn: () => getSingleNote(id),
   });
 
@@ -49,3 +29,22 @@ const NoteDetails = async ({ params }: Props) => {
 };
 
 export default NoteDetails;
+
+// Прямой вход на /notes/id
+
+// → новый HTML документ
+// → Next.js строит страницу как полноценный route
+// → metadata может быть подготовлена до отдачи HTML
+// → для HTML limited bots она будет в head
+
+// Клик внутри приложения
+// → document не перезагружается
+// → идет RSC payload, а не полный HTML
+// → модалка вставляется в текущее дерево
+// → metadata может появиться в body
+
+// Запрос на сервер при Link есть, но это не тот же самый запрос, что при refresh.
+// Refresh просит у сервера новый HTML документ со своим head.
+// Link просит у сервера только данные нового маршрута, чтобы обновить React дерево без перезагрузки страницы.
+// Поэтому при refresh metadata может попасть в head, а при client navigation или interception route она
+// может быть добавлена позже и оказаться в body.
